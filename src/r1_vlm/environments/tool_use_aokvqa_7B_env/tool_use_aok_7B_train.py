254b1a05a0fa4eac20cb8ff882f8a5cdc738418a
Sunil Kumar <sdkumar@hmc.edu>
2025-04-23 12:57:04 -0700

    fix out dir

--- src/r1_vlm/environments/tool_use_aokvqa_7B_env/tool_use_aok_7B_train.py
+++ src/r1_vlm/environments/tool_use_aokvqa_7B_env/tool_use_aok_7B_train.py
@@ -105,7 +105,7 @@ def train():
     training_args = GRPOConfig(
         model_init_kwargs=model_config,
         # save path on the runpod instance
-        output_dir="workspace/vlm-r1-tool-use-aokvqa-env",
+        output_dir="/workspace/vlm-r1-tool-use-aokvqa-env",
         # increase learning rate for PEFT - 1e-4
         learning_rate=1e-4 if peft_config is not None else 1e-6,
         adam_beta2=0.98,
