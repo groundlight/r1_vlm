193487e02d12f886ac581157f2e9425c6cb51bb8
Sunil Kumar <sdkumar@hmc.edu>
2025-04-29 15:55:22 +0000

    swap back

--- src/r1_vlm/environments/tool_use_aokvqa_env/eval.py
+++ src/r1_vlm/environments/tool_use_aokvqa_env/eval.py
@@ -20,9 +20,7 @@ def extract_answer(generation: str):
 
 
 def main():
-    checkpoint = (
-        "/millcreek/home/sunil/r1_vlm_bumbershoot2/r1_vlm/checkpoint-500-better-zoom"
-    )
+    checkpoint = "/millcreek/home/sunil/r1_vlm/vlm-r1-zoom-only-reward-refactor-oversampling/checkpoint-700"
     processor = AutoProcessor.from_pretrained(checkpoint, padding_side="left")
     vf_env = AOKVQAToolEnv(processing_class=processor)
     train_dataset, val_dataset, test_dataset = vf_env.get_dataset()
@@ -38,7 +36,7 @@ def main():
         )
 
         sampling_params = SamplingParams(
-            temperature=1.0,
+            temperature=0.1,
             max_tokens=2048,
         )
 
