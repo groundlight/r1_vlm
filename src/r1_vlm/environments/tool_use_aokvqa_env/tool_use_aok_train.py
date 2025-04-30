af03c8c48b4d273a1f150aaa93b8fa2e5e1c7026
Sunil Kumar <sdkumar@hmc.edu>
2025-04-29 17:48:14 -0700

    ready to train

--- src/r1_vlm/environments/tool_use_aokvqa_env/tool_use_aok_train.py
+++ src/r1_vlm/environments/tool_use_aokvqa_env/tool_use_aok_train.py
@@ -106,7 +106,7 @@ def train():
     training_args = GRPOConfig(
         model_init_kwargs=model_config,
         # save path on the runpod instance
-        output_dir="vlm-r1-new-zoom-tool-reward-independent-oversampling",
+        output_dir="vlm-r1-new-od-tool-reward-schedule-for-tools-apr-29",
         # increase learning rate for PEFT - 1e-4
         learning_rate=1e-4 if peft_config is not None else 1e-6,
         max_grad_norm=1.0,
