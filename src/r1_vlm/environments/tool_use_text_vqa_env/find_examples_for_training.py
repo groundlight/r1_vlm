40be9acfbf2d6aa049dbced2c49326286944c690
Sunil Kumar <sdkumar@hmc.edu>
2025-05-12 15:07:21 -0700

    mixed dataset

--- src/r1_vlm/environments/tool_use_text_vqa_env/find_examples_for_training.py
+++ src/r1_vlm/environments/tool_use_text_vqa_env/find_examples_for_training.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 # chooses examples from the training set that are good candidates for training with tool use.
 
@@ -37,5 +38,48 @@ def find_examples_for_training(
     return question_ids
 
 
+def find_examples_for_training_1_to_1_mix(
+    eval_results_file_path: str = "/millcreek/home/sunil/r1_vlm/src/r1_vlm/environments/tool_use_text_vqa_env/eval_on_train_results.jsonl",
+):
+    """
+    Returns a dataset that is a 1:1 mix of easy to hard examples, where
+    easy == vqa score > 0
+    hard == vqa score == 0
+    """
+
+    results = []
+    if os.path.exists(eval_results_file_path):
+        with open(eval_results_file_path, "r") as f:
+            results = [json.loads(line) for line in f]
+    else:
+        raise FileNotFoundError(f"File {eval_results_file_path} not found")
+
+    print(f"There are {len(results)} results in the file {eval_results_file_path}")
+
+    easy_results = [result for result in results if result["vqa_score"] > 0]
+    hard_results = [result for result in results if result["vqa_score"] == 0]
+
+    # there should be more easy than hard
+
+    if not len(easy_results) > len(hard_results):
+        raise ValueError(
+            f"There should be more easy than hard results, but there are {len(easy_results)} easy and {len(hard_results)} hard"
+        )
+
+    # take all the hard results, and then sample without replacement from the easy results until we have the same number of easy and hard results
+    sampled_easy_results = random.sample(easy_results, len(hard_results))
+
+    # combine the results and shuffle
+    combined_results = hard_results + sampled_easy_results
+    random.shuffle(combined_results)
+
+    # now take their question ids
+    question_ids = [result["question_id"] for result in combined_results]
+
+    return question_ids
+
+
 if __name__ == "__main__":
     find_examples_for_training()
+
+    find_examples_for_training_1_to_1_mix()
